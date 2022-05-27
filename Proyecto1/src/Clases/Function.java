c406ccfdc26f490e13a66a60984362d2f595ed6c
Clossedleon <97573404+Clossedleon@users.noreply.github.com>
2022-05-27 14:58:48 -0400

    Add files via upload

---
+++ Proyecto1/src/Clases/Function.java
@@ -0,0 +1,53 @@
+
+package Clases;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+
+public class Function {
+    
+    public void cargar_archivo() {
+        
+    }
+    
+    public void leer_archivo() {
+        
+    }
+    
+    public void guardar_archivo() {
+        
+    }
+    
+    public void imprimir_inventario() {
+        
+    }
+    
+    public void imprimir_productos() {
+        
+    }
+    
+    public void realizar_pedido() {
+        
+    }
+    
+    public void solicitar_productos() {
+        
+    }
+    
+    public void agregar_almacen() {
+        
+    }
+    
+    public void gestionar_almacen() {
+        
+    }
+    
+    public void mostrar_grafo() {
+        
+    }
+    
+}
---
+++ Proyecto1/src/Clases/Main.java
@@ -0,0 +1,15 @@
+
+package Clases;
+
+import Interfaces.MenuPrincipal;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        MenuPrincipal test = new MenuPrincipal();
+        
+    }
+    
+}
