86626ab638fd3ba40bbd88a79b419d5b1dd57bf1
martinjup <97602564+martinjup@users.noreply.github.com>
2022-06-05 15:03:58 -0400

    Add files via upload

--- Proyecto1/src/Interfaces/ManageStorage.java
+++ Proyecto1/src/Interfaces/ManageStorage.java
@@ -5,6 +5,8 @@
  */
 package Interfaces;
 
+import Clases.Global;
+
 /**
  *
  * @author Windows
@@ -22,7 +24,7 @@ public class ManageStorage extends javax.swing.JFrame {
         this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -36,6 +38,18 @@ public class ManageStorage extends javax.swing.JFrame {
         jTextArea1 = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        DisplayAlmacenes = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        DisplayInventarios = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        InputAlmacen = new javax.swing.JTextField();
+        InputProducto = new javax.swing.JTextField();
+        InputCantidad = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -48,6 +62,70 @@ public class ManageStorage extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 102));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        DisplayAlmacenes.setColumns(20);
+        DisplayAlmacenes.setRows(5);
+        jScrollPane2.setViewportView(DisplayAlmacenes);
+        Integer nalmacenes = Global.getLista_almacenes().getLength();
+
+        Integer i = 0;
+
+        String almacenes = "";
+
+        while (i < nalmacenes ){
+
+            almacenes += Global.getLista_almacenes().findName(i);
+            almacenes += "\n";
+
+            i++;
+
+        }
+        DisplayAlmacenes.setText(almacenes);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 40, 220, 200));
+
+        DisplayInventarios.setColumns(20);
+        DisplayInventarios.setRows(5);
+        jScrollPane3.setViewportView(DisplayInventarios);
+
+        jPanel1.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 40, 220, 200));
+
+        jLabel1.setText("Almacenes");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 10, -1, -1));
+
+        jLabel2.setText("Inventario");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 10, -1, -1));
+
+        InputAlmacen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InputAlmacenActionPerformed(evt);
+            }
+        });
+        jPanel1.add(InputAlmacen, new org.netbeans.lib.awtextra.AbsoluteConstraints(54, 260, 220, -1));
+
+        InputProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InputProductoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(InputProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(344, 270, 210, -1));
+
+        InputCantidad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InputCantidadActionPerformed(evt);
+            }
+        });
+        jPanel1.add(InputCantidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(344, 330, 210, -1));
+
+        jLabel3.setText("Producto");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 250, -1, -1));
+
+        jLabel4.setText("Cantidad");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 310, -1, -1));
+
+        jLabel5.setText("Elija Almacen");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 240, -1, -1));
+
         jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 670, 380));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -63,10 +141,26 @@ public class ManageStorage extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    private void InputProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InputProductoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InputProductoActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void InputCantidadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InputCantidadActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InputCantidadActionPerformed
+
+    private void InputAlmacenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InputAlmacenActionPerformed
+
+        
+        String almacen = InputAlmacen.getText();
+        Integer posicion = Global.getLista_almacenes().findPosition(almacen);
+        DisplayInventarios.setText(Global.getLista_almacenes().findNodo(posicion).getListaItems());
+    
+        
+        
+    }//GEN-LAST:event_InputAlmacenActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -100,9 +194,21 @@ public class ManageStorage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea DisplayAlmacenes;
+    private javax.swing.JTextArea DisplayInventarios;
+    private javax.swing.JTextField InputAlmacen;
+    private javax.swing.JTextField InputCantidad;
+    private javax.swing.JTextField InputProducto;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
